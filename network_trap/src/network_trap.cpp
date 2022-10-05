b9cc4c43baaccdd8f27810d543c6ac5367079947
ayaanle <ayaanlemohamed540@gmail.com>
2022-10-05 12:04:10 +0300

    net

---
+++ network_trap/src/network_trap.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : network_trap.cpp
+// Author      : Ayanle
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
