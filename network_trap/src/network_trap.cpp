725084a456466d8f30b20fec2dcc2b8dbeda29bd
ayaanle <ayaanlemohamed540@gmail.com>
2022-10-05 12:14:55 +0300

    net

--- network_trap/src/network_trap.cpp
+++ network_trap/src/network_trap.cpp
@@ -6,10 +6,35 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-using namespace std;
 
+//============================================================================
+// Name        : ayanlaz.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/filesystem.hpp>
+#include <map>
+
+namespace Connections{
+  template <typename IPV4 , typename IPV6> struct Net{
+  public:
+	  Net(IPV4);//normal constructor
+	  Net(IPV4 &); //copy constructor
+	  ~Net();
+  public:
+	  double _Bandwidth;
+	  volatile double _Signal_Strength;
+
+  };
+
+}
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	std::cout << "this project is about to start" << std::endl; // prints !!!Hello World!!!
 	return 0;
 }
+
